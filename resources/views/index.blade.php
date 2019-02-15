505a82412b38264aa65bc9284c36abd17e66767c
agusprasetyo1 <agusprasetyo1889@gmail.com>
2019-02-15 21:13:01 +0700

    add blade template

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('page-title', 'Dashboard')
+
+@section('breadcrumb')
+    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+    <li class="breadcrumb-item active"></li>
+@endsection
+
+@section('content')
+    <h1>Ini bagian content</h1>
+@endsection
