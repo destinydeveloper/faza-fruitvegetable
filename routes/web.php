1088ae79ac8aac8c192f27bb65b983730ac6a16f
agusprasetyo1 <agusprasetyo1889@gmail.com>
2019-02-16 09:34:46 +0700

    add blade template (login & register)

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::get('/', function () {
 
 Route::get('/login', function () {
     return view('login');
+})->name('data.login');
+
+Route::get('/register', function () {
+    return view('register');
 });
