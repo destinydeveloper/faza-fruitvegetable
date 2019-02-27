46519e6e8bd108cbf8c0e7ef6813b61de17a6ca6
viandwi24 <fiandwi0424@gmail.com>
2019-02-27 22:17:53 +0700

    b-1.2.1

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth.role:REDIRECT_HOME_PAGE');
-// Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home')->middleware('auth.role:REDIRECT_HOME_PAGE');
+Route::get('/home', 'HomeController@index')->name('home');
 
 
 /**
