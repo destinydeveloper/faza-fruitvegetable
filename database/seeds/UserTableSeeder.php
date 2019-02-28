6693a90f9535b13dea9d8ccc0e5a9ba051657a07
viandwi24 <fiandwi0424@gmail.com>
2019-02-28 20:08:19 +0700

    Added Customer Account

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,11 +11,20 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
+        echo "Create Admin Account...\n";
         $user_level = \App\Models\Admin::create([]);
         $user = new \App\User();
         $user->name = 'MASTER ADMIN';
         $user->email = 'admin@mail.com';
         $user->password = bcrypt('admin');
+        $user_level->user()->save($user);
+
+        echo "Create Customer Account...\n";
+        $user_level = \App\Models\Customer::create([]);
+        $user = new \App\User();
+        $user->name = 'Customer Demo';
+        $user->email = 'user@mail.com';
+        $user->password = bcrypt('user');
         $user_level->user()->save($user);
     }
 }
